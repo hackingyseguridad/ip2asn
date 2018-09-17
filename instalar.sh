78b4d7eae52febcd61bfbd6a0ca4718ac96935b0
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-09-17 23:28:48 +0200

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo
+echo instalador 
+echo Se instalaran as utilidades en /sbin/ para poder ser ejecutardo como comandos de Linux
+echo
+chmod 777 *
+cp * /sbin/
+echo
+echo "Instalacion finalizada."
+echo
